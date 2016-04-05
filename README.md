9657c723b2942f04c3a4b502c53fdee43fe8ce4f
Syeda <Syeda@rubab.local>
2016-04-04 19:22:59 -0500

    Final Update with new design docs

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
-# facility-service
-CS 473
+# facility-service #
 
+### GitHub Repository ###
 
-Database DDL statements are available inside database.txt. 
-You can run it from command line using following command (change path to this file path, mysql server needs to be started before that):
+https://github.com/snowreen/facility-management
+
+### Implementation ###
+
+I used JDBC in previous project. To change it into Hibernate ORM, I have created “hibernate.cfg.xml” which has the session factory configuration to connect to MYSQL database and all hibernate mapping file locations.
+
+Hibernate mapping files, *.hbm.xml, specifies the mapping between Java class and the database table, also has relationships defined between classes.
+
+HibernateMYSQLHelper class is a helper class, which initializes a static final SessionFactory object. 
+
+All the DAOs have the methods to query database. For fetching data, I have used mostly HQL.
+
+Now, to have loose coupling in project, I have created interfaces. Then I have added Spring beans inside “resources/META-INF/app-context.xml” and injected dependencies using “setter” methods.  For scope, most of them have “prototype” scope.
+
+Then in FacilityClient class, I have created a static object of ApplicationContext by reading app-context.xml from classpath, so that I can get an object of a bean from applicationContext by the bean id.
+
+### How to Run the Project ###
+
+All spring and hibernate libraries can be found inside libs folder. Please add those in your classpath after importing in IDE.
+
+Database DDL statements are available inside database.txt (attached with email and also available in Github https://github.com/snowreen/facility-management) . You can run it from command line using following command (change path to this file path, mysql server needs to be started before that):
 
 mysql < {path}/database.txt
 
-Please change mysql database username and password according to yours in DBHelper.java class (line 33).
+Please change mysql database username and password according to yours in hibernate.cfg.xml file.
+
+### For Testing ###
 
 FacilityClient.java has the main method to test this application.
--- src/main/java/com/facility/management/model/usage/LeaseInfoImpl.java
+++ src/main/java/com/facility/management/model/usage/LeaseInfoImpl.java
@@ -31,130 +31,82 @@ public class LeaseInfoImpl implements LeaseInfo {
 		this.terminationDate = terminationDate;
 		this.status = status;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#getLeaseId()
-	 */
+	
 	@Override
 	public Integer getLeaseId() {
 		return this.leaseId;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#setLeaseId(java.lang.Integer)
-	 */
+	
 	@Override
 	public void setLeaseId(Integer leaseId) {
 		this.leaseId = leaseId;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#getFacility()
-	 */
+	
 	@Override
 	public Facility getFacility() {
 		return this.facility;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#setFacility(com.facility.management.model.Facility)
-	 */
+	
 	@Override
 	public void setFacility(Facility facility) {
 		this.facility = facility;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#getTenant()
-	 */
+	
 	@Override
 	public Tenant getTenant() {
 		return this.tenant;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#setTenant(com.facility.management.model.TenantImpl)
-	 */
+	
 	@Override
 	public void setTenant(Tenant tenant) {
 		this.tenant = tenant;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#getStartDate()
-	 */
+	
 	@Override
 	public Date getStartDate() {
 		return this.startDate;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#setStartDate(java.util.Date)
-	 */
+	
 	@Override
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#getEndDate()
-	 */
+	
 	@Override
 	public Date getEndDate() {
 		return this.endDate;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#setEndDate(java.util.Date)
-	 */
+	
 	@Override
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#getSecurityDeposite()
-	 */
+	
 	@Override
 	public BigDecimal getSecurityDeposite() {
 		return this.securityDeposite;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#setSecurityDeposite(java.math.BigDecimal)
-	 */
+	
 	@Override
 	public void setSecurityDeposite(BigDecimal securityDeposite) {
 		this.securityDeposite = securityDeposite;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#getTerminationDate()
-	 */
+	
 	@Override
 	public Date getTerminationDate() {
 		return this.terminationDate;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#setTerminationDate(java.util.Date)
-	 */
+	
 	@Override
 	public void setTerminationDate(Date terminationDate) {
 		this.terminationDate = terminationDate;
 	}
-
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#getStatus()
-	 */
+	
 	@Override
 	public String getStatus() {
 		return this.status;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.facility.management.model.LeaseInfo#setStatus(java.lang.String)
-	 */
 	@Override
 	public void setStatus(String status) {
 		this.status = status;
